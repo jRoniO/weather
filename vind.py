a9880a3652b20784b2eac24de9844c396083f11b
jRoniO <ronoma1016@skola.goteborg.se>
2022-10-04 12:41:41 +0100

    inga negativa tecken

--- vind.py
+++ vind.py
@@ -2,20 +2,21 @@
 #!/usr/bin/env python3
 
 import serial
-if __name__ == '__main__':
-    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
-    ser.reset_input_buffer()
-    while True:
-        if ser.in_waiting > 0:
-            line = ser.readline().decode('utf-8').rstrip()
-            print(line)#!/usr/bin/env python3
-'''
 import time
-import serial
 
-windPrint = ""
-while 1:
-        ser = serial.Serial('/dev/ttyACM0', 9600)
-print(f'\rVindhastigheten är: {windPrint}', end='')
-time.sleep(1)
-'''
+ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+ser.reset_input_buffer()
+
+while True:
+	line = ser.readline().decode('utf-8').rstrip()
+	
+	if line:
+		remove_negative = line.replace('-','0,00')
+		remove_decimal = remove_negative[0:4]
+		print(f'Vindhastigheten är %s m/s' % remove_decimal)
+		time.sleep(1)
+
+	else:
+		print(f'Vindhastigheten är %s m/s' % line)
+		time.sleep(1)
+		
